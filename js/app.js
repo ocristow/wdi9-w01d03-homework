2392d559a37ada28d9072bfeda95df82e6324800
Oliver Ristow <ocristow@gmail.com>
2018-12-12 13:39:25 -0700

    Fizz Buzz answered

--- js/app.js
+++ js/app.js
@@ -11,11 +11,27 @@
 // }
 
 //Excited Kitten
-for (let i = 0; i <= 20; i++) {
-    console.log("Love me, pet me! HSSSSSS!")
-} if (i % 2 === 0) {
-    console.log("...human...why you taking pictures of me?");
-    console.log("...the catnip made me do it...");
-    console.log("...why does the red dot always get away...");
-}
+// for (let i = 0; i <= 20; i++) {
+//     console.log("Love me, pet me! HSSSSSS!")
+// } if (i % 2 === 0) {
+//     console.log("...human...why you taking pictures of me?");
+//     console.log("...the catnip made me do it...");
+//     console.log("...why does the red dot always get away...");
+// }
 //look up Math.random
+
+//Fizzbuzz
+ fizzBuzz = function() {
+    for (let i = 0; i <= 100; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log("FizzBuzz");
+        } else if (i % 3 === 0)
+            console.log("Fizz");
+        else if (i % 5 === 0)
+            console.log("Buzz");
+        else 
+            console.log(i);
+    }
+}
+
+fizzBuzz();
