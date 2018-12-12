35f37dee20d74dcb03aa80fc39ede27050e88f7c
Oliver Ristow <ocristow@gmail.com>
2018-12-12 13:12:23 -0700

    Commit 2 - Get Even answered

--- js/app.js
+++ js/app.js
@@ -1,4 +1,11 @@
 // DAY 3 HOMEWORK 
-for (let i = 1; i <= 20; i++) {
-    console.log("Counting up from " + i);
+
+//Easy Going
+// for (let i = 1; i <= 20; i++) {
+//     console.log("Counting up from " + i);
+// }
+
+//Get Even
+for (let i = 0; i <= 200; i+=2) {
+    console.log("Even numbers only! Logging: " + i)
 }
