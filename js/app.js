41927a92f73aa8f3f97da2d0ad79ae8fe20c0888
Oliver Ristow <ocristow@gmail.com>
2018-12-12 13:17:26 -0700

    Excited Kittens answered

--- js/app.js
+++ js/app.js
@@ -6,6 +6,16 @@
 // }
 
 //Get Even
-for (let i = 0; i <= 200; i+=2) {
-    console.log("Even numbers only! Logging: " + i)
+// for (let i = 0; i <= 200; i+=2) {
+//     console.log("Even numbers only! Logging: ")
+// }
+
+//Excited Kitten
+for (let i = 0; i <= 20; i++) {
+    console.log("Love me, pet me! HSSSSSS!")
+} if (i % 2 === 0) {
+    console.log("...human...why you taking pictures of me?");
+    console.log("...the catnip made me do it...");
+    console.log("...why does the red dot always get away...");
 }
+//look up Math.random
