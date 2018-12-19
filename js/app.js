515cce879494508820428ec16f67e29b41ae16b7
Oliver Ristow <ocristow@gmail.com>
2018-12-19 06:58:45 -0700

    yell at the ninja turtles answered

--- js/app.js
+++ js/app.js
@@ -38,61 +38,45 @@
 
 
 //5. GETTING TO KNOW YOU
-const shahzad = ["Shahzad", 1000, "Austin"];
-const jim = ["Jim", 16, "All cities"];
-const reuben = ["Reuben", 22, "Durham"];
-const daniel = ["Daniel", 186, "Dallas"];
-const ryan = ["Ryan", 65, "Denver"];
+// const shahzad = ["Shahzad", 1000, "Austin"];
+// const jim = ["Jim", 16, "All cities"];
+// const reuben = ["Reuben", 22, "Durham"];
+// const daniel = ["Daniel", 186, "Dallas"];
+// const ryan = ["Ryan", 65, "Denver"];
 
-shahzad.splice(0, 1, 'Gameboy');
-console.log(shahzad);
+// shahzad.splice(0, 1, 'Gameboy');
+// console.log(shahzad);
 
-console.log(daniel[1]);
-daniel[1] += 1
-console.log(daniel[1]);
+// console.log(daniel[1]);
+// daniel[1] += 1
+// console.log(daniel[1]);
 
-console.log(ryan[2]);
-ryan.splice(2, 1, 'Gotham City');
-console.log(ryan[2])
+// console.log(ryan[2]);
+// ryan.splice(2, 1, 'Gotham City');
+// console.log(ryan[2])
 
-console.log(reuben);
-reuben.pop();
-console.log(reuben);
-reuben.push('Chicago');
-console.log(reuben);
+// console.log(reuben);
+// reuben.pop();
+// console.log(reuben);
+// reuben.push('Chicago');
+// console.log(reuben);
 
-console.log(jim);
-jim.pop();
-console.log(jim);
-jim.push('Nome', 'Djibouti', 'Atlantis')
-console.log(jim);
-jim.splice(2, 1);
-console.log(jim);
-
-
-//6.
-
-//7. Yell at the ninja turtles
+// console.log(jim);
+// jim.pop();
+// console.log(jim);
+// jim.push('Nome', 'Djibouti', 'Atlantis')
+// console.log(jim);
+// jim.splice(2, 1);
+// console.log(jim);
 
 
-// Create an array with the members of the ninja turtles (Donatello, Leonardo, Raphael, Michaelangelo)
+// 6. NINJA TURTLES
+let ninjaTurt = ['Donatello', 'Leonardo', 'Raphael', 'Michaelangelo'];
 
-// Use a for loop to call .toUpperCase() on each of them and print out the result.
-
-// Bonus: Modify the answer you just wrote. Instead of all letters being uppercase, 
-// make the letters alternate back and forth between uppercase and lowercase.
-
-// let ninjaT = ["Donatello", "Leonardo", "Raphael", "Michaelangelo"];
-
-// for (let i = 0; i < ninjaT.length; i++) {
-// let currentNT = ninjaT[i].split("");
-// console.log(currentNT);
-// }
-
-//Create an array with the ninja turtles.
-//Cut them all out of the array so that they are their own objects, store them in different variables.
-//Split their letters apart and perform the toUppercase() on the letters.
-//
+for (let i = 0; i < ninjaTurt.length; i++) {
+    let currentNT = ninjaTurt[i];
+    console.log(currentNT.toUpperCase());
+}
 
 
 
@@ -100,15 +84,9 @@ console.log(jim);
 
 
 
-// ninTurt = [Donatello, Leonardo, Raphael, Michaelangelo];
-// for (let i = 0; i < ninTurt.length; i++){
-//     let thisTurt = ninTurt[i].split("");
-//     //console.log(thisTurt[3]);
-//     // try split and join!!!
-//         for (let t = 2; t < thisTurt.length; t += 2){
-//             let thisTurtLetter = thisTurt[t];
-//             thisTurtLetter.toUpperCase();
-//             console.log(thisTurtLetter);
+
+
+
 
 
 
